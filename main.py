e1ed30b457e8f69ad25d14d5cc2cc6244f1be317
RichieLee93 <lrq19930222@gmail.com>
2018-09-05 15:58:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ from tkinter import messagebox
 
 
 def search_it():
+    """
+    print wordcloud and search results based on the paper titiles
+    
+    """
     author_id = get_author_id(str(name.get()))
     for aid in author_id:
         author_paper,paper_detail = get_author_paper(aid)
@@ -17,27 +21,25 @@ def search_it():
         paper_detail = paper_detail.replace(']','')
         paper_detail = paper_detail.replace(',','\r\n')
         # root.option_add("*Dialog.msg.wrapLength", "10i")
-        messagebox.showinfo(name.get(), paper_detail)
+        messagebox.showinfo(name.get(), paper_detail) # print the search results by popping out messagebox
         #blank.insert(0, paper_detail)
         token_list = preprocess(author_paper)
         token_freq = FreqDist(token_list)
         wc = WordCloud(background_color="white").generate_from_frequencies(token_freq)
-        plt.imshow(wc, interpolation='bilinear')
+        plt.imshow(wc, interpolation='bilinear') # show wordcloud
         plt.axis("off")
         plt.show()
-
-
 
 #end def
 
 
-root = Tk()
-root.title('Semantic Scholar Search Engine')
-root.geometry('640x640+0+0')
-heading = Label(root, text='Semantic Scholar Search Engine', font=('arial',30,'bold'), fg='steelblue').pack()
-label = Label(root, text='Enter the author name:',font=('arial',20,'bold'), fg='black').place(x=10,y=120)
+root = Tk() # create a now application form
+root.title('Semantic Scholar Search Engine') # set app name
+root.geometry('640x640+0+0') # set the app window size
+heading = Label(root, text='Semantic Scholar Search Engine', font=('arial',30,'bold'), fg='steelblue').pack() # set app head title
+label = Label(root, text='Enter the author name:',font=('arial',20,'bold'), fg='black').place(x=10,y=120) # set text label size and title
 name = StringVar()
-entry_box = Entry(root, textvariable=name, width=18, bg='orange').place(x=260,y=120)
-search = Button(root, text='search', width=10, height=3, bg='blue',command=search_it).place(x=290, y=170)
+entry_box = Entry(root, textvariable=name, width=18, bg='orange').place(x=260,y=120) # set entry box settings
+search = Button(root, text='search', width=10, height=3, bg='blue',command=search_it).place(x=290, y=170) # set search button settings
 
 root.mainloop()
